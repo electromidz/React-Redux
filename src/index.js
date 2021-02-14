ab62b0e6a377383b810d0244c403633a6939f68d
electromidz <electromidz@gmail.com>
2021-02-14 23:24:59 +0330

    some code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
+import reactDom from 'react-dom';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'; 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import App from './components/App';
+import { reducers } from './reducers';
+
+
+
+reactDom.render(
+<Provider store={createStore(reducers)}><App/></Provider>, 
+document.querySelector('#root'));
